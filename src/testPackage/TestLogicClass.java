e4f85fc0969ccdae37729b105811d812293984b0
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 06:28:38 +0530

    Added the new changes by second user is new branch

--- src/testPackage/TestLogicClass.java
+++ src/testPackage/TestLogicClass.java
@@ -5,7 +5,7 @@ public class TestLogicClass
 	public void testLoginMethod()
 	{
 		System.out.println("This is logic of login code");
-		System.out.println("Enter the username");
-		System.out.println("Entet the Password");
+		System.out.println("Enter the username 1");
+		System.out.println("User should directly logged in with SSO Credentials");
 	}
 }
