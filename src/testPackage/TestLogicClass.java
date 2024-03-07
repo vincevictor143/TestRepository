f6643494208e058cf72f9cc56326bf3f27d3e888
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 06:22:48 +0530

    Adding the login logic to main method

---
+++ src/testPackage/TestLogicClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestLogicClass 
+{
+	public void testLoginMethod()
+	{
+		System.out.println("This is logic of login code");
+		System.out.println("Enter the username");
+		System.out.println("Entet the Password");
+	}
+
+}
