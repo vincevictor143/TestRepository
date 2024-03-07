853377a69e49aed78ab2cc3a50435780982ce017
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-07 06:16:02 +0530

    Adding testclass 1 and testclass2

---
+++ src/testPackage/TestClass.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class TestClass 
+{
+	public void testMethod()
+	{
+		System.out.println("Adding the first test class");
+	}
+
+}
---
+++ src/testPackage/TestClass2.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class TestClass2 
+{
+	public void testMethod2()
+	{
+		System.out.println("Adding test class2");
+	}
+
+}
